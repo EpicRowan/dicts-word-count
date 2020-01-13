e96b345032db72d5174d52afb1d3b9de833a79aa
Your Name <you@example.com>
2020-01-13 12:24:23 -0800

    Step one works

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,20 @@
 # put your code here.
+
+def count_da_words(file_name):
+    """ """
+
+    the_test =open(file_name)
+
+    word_counts = {}
+
+    for line in the_test:
+        line = line.rstrip()
+        # line = line.split('')
+
+    for word in line:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    return word_counts
+
+
+print(count_da_words("test.txt"))
